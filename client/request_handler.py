d06879237ad408b61bf4edb3de6f6c16f76cef55
t3hpaul <pstasiuk@lwsDevDesktop.(none)>
2012-11-25 16:41:33 -0600

    Added modules for registration, fixed small issues with changes being committed to the database.

--- client/request_handler.py
+++ client/request_handler.py
@@ -7,7 +7,7 @@ import time as time
 import json
 
 #puts a temperature change onto the server
-def put_temp_change(temp, phidget_id, sensor_id):
+def put_value_change(value, phidget_id, sensor_id):
 	headers = {'content-type':'application/json'}
 	temp_data = {
 			 'd':datetime.now().day,
@@ -16,8 +16,8 @@ def put_temp_change(temp, phidget_id, sensor_id):
 		         'h':datetime.now().hour,
 		         'min':datetime.now().minute,
 		         's':datetime.now().second,
-			 #'ms':int(round(time_.time()*1000), 
-		         'val':temp,
+			 #'ms':int(round(time_.time()*1000)), 
+		         'val':value,
 		         'phid':phidget_id,
 		         'sensid':sensor_id,
 		     }
@@ -29,7 +29,7 @@ def put_temp_change(temp, phidget_id, sensor_id):
 	#request.add_header('Content-Type', 'application/json')
 	#request.get_method = lambda: 'PUT'
 	#url = opener.open(request)
-	print 'putting the request!'
+	#print 'putting the request!'
 	temp_data=json.dumps(temp_data)
 
 	the_request = requests.put('http://127.0.0.1:5000/devices/updates/temp', data=temp_data, headers=headers)
