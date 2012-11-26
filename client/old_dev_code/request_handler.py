fa38e214014e319802e71d87f6544fe6d944713e
t3hpaul <pstasiuk@lwsDevDesktop.(none)>
2012-11-25 20:42:06 -0600

    Device id generation added(repeatable process). Small tweaks to get device ID and changes to the fake_client which allow us to to connect to the server and test it without actually using a phidget

--- client/old_dev_code/request_handler.py
+++ client/old_dev_code/request_handler.py
@@ -7,7 +7,7 @@ import time as time
 import json
 
 #puts a temperature change onto the server
-def put_value_change(value, phidget_id, sensor_id):
+def put_temp_change(temp, phidget_id, sensor_id):
 	headers = {'content-type':'application/json'}
 	temp_data = {
 			 'd':datetime.now().day,
@@ -17,7 +17,7 @@ def put_value_change(value, phidget_id, sensor_id):
 		         'min':datetime.now().minute,
 		         's':datetime.now().second,
 			 #'ms':int(round(time_.time()*1000)), 
-		         'val':value,
+		         'val':temp,
 		         'phid':phidget_id,
 		         'sensid':sensor_id,
 		     }
@@ -29,32 +29,7 @@ def put_value_change(value, phidget_id, sensor_id):
 	#request.add_header('Content-Type', 'application/json')
 	#request.get_method = lambda: 'PUT'
 	#url = opener.open(request)
-	#print 'putting the request!'
+	print 'putting the request!'
 	temp_data=json.dumps(temp_data)
 
 	the_request = requests.put('http://127.0.0.1:5000/devices/updates/temp', data=temp_data, headers=headers)
-	
-
-
-#Function that registeres the device, will return a JSON object if the device has not been registered, 1 if registration
-#has been sucessful, 2 if the device has already been registered.
-#Data looks like:
-#reg_info:
-
-def register_device(ip_address, dev_id):
-	reg_info = {
-			'ip_address':ip_address,
-			'dev_id':dev_id,
-			'd':datetime.now().day,
-                        'y':datetime.now().year,
-                        'month':datetime.now().month,
-                        'h':datetime.now().hour,
-                        'min':datetime.now().minute
-		}
-	
-	headers = {'content-type':'application/json'}
-	temp_data=json.dumps(reg_info)
-	the_request = requests.put('http://127.0.0.1:5000/devices/register', data=reg_info, headers=headers)
-	
-
-
