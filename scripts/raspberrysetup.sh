dc53971b4e1a00f925a25c72efdddd8fc5ff53ed
root <root@raspberrypi.(none)>
2013-03-04 10:23:28 -0600

    Added the general setup script

---
+++ scripts/raspberrysetup.sh
@@ -0,0 +1,39 @@
+rm -r Desktop/
+rm ocr_pi.png 
+rm -r python_games/
+echo "Getting update"
+apt-get install update
+echo "Installing git"
+apt-get install git
+echo "Installing dependencies for phidgets!"
+apt-get install libusb-1.0-0-dev
+echo "Installing the phidgets!"
+wget http://www.phidgets.com/downloads/libraries/libphidget.tar.gz
+tar -xzvf libphidget.tar.gz 
+rm libphidget.tar.gz
+cd libphidget-2.1.8.20121218/
+./configure
+make
+make install
+cd ..
+rm -r libphidget-2.1.8.20121218
+wget http://www.phidgets.com/downloads/libraries/PhidgetsPython.zip
+unzip PhidgetsPython.zip
+cd PhidgetsPython
+python setup.py install
+echo "Installing requests framework"
+cd ..
+rm -r PhidgetsPython
+git clone git://github.com/kennethreitz/requests.git
+cd requests
+python setup.py install
+cd ..
+rm -r requests
+echo "cloning everything to its correct positions"
+git clone https://github.com/t3hpaul/lws.git /etc/lws
+cp /etc/lws/scripts/initscript.sh /etc/init.d/lwsclient
+echo "Setting up the startup scripts"
+cat /etc/lws/scripts/initscript.sh > /etc/init.d/lwsclient
+chmod 777 /etc/init.d/lwsclient
+echo "Now testing the installation."
+python /etc/lws/client/lws_client_main.py
